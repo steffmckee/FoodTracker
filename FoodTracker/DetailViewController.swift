f19a8a3865edce59adddd7305ae5d6a6bacfd2b4
Steff McKee <steffmckee@outlook.com>
2017-01-17 20:45:56 -0700

    added DetailViewController

---
+++ FoodTracker/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  FoodTracker
+//
+//  Created by Steff McKee on 1/17/17.
+//  Copyright © 2017 Steff McKee. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func eatItBarButtonItemPressed(_ sender: UIBarButtonItem) {
+    }
+    
+}
