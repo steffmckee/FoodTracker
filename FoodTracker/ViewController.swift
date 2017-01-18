4cb7b801afb92670c49aee4bf3f6e3e555a87302
Steff McKee <steffmckee@outlook.com>
2017-01-17 20:49:48 -0700

    added search controller and delegates

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, UISearchControllerDelegate, UISearchResultsUpdating {
 
     @IBOutlet weak var tableView: UITableView!
+    var searchController:UISearchController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +23,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // Mark - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    // Mark - UISearchResultsUpdating
+    
+    func updateSearchResults(for searchController: UISearchController) {
+    }
+    
 }
 
